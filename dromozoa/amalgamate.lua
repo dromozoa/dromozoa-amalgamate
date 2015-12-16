3c1c213ce321dd74f3afd671557dfa3982354588
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-12-16 12:52:15 +0900

    結果を修正。

--- dromozoa/amalgamate.lua
+++ dromozoa/amalgamate.lua
@@ -15,6 +15,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-amalgamate.  If not, see <http://www.gnu.org/licenses/>.
 
+local empty = require "dromozoa.commons.empty"
 local loadstring = require "dromozoa.commons.loadstring"
 local read_file = require "dromozoa.commons.read_file"
 local searchpath = require "dromozoa.commons.searchpath"
@@ -22,107 +23,32 @@ local sequence = require "dromozoa.commons.sequence"
 local sequence_writer = require "dromozoa.commons.sequence_writer"
 local unpack = require "dromozoa.commons.unpack"
 
+local searchers
+if _VERSION >= "Lua 5.2" then
+  searchers = package.searchers
+else
+  searchers = package.loaders
+end
+
 local exit_success = function () end
 
-local function copy(this, that)
-  for k, v in pairs(that) do
-    this[k] = v
+local prolog = [[
+local dromozoa_amalgamate_package_loaded = {}
+for k, v in pairs(package.loaded) do
+  dromozoa_amalgamate_package_loaded[k] = v
+end
+]]
+
+local function clean(code)
+  code = code:gsub("^%#%![^\n]*\n+", "")
+  local n = #prolog
+  if code:sub(1, n) == prolog then
+    code = code:sub(n + 1)
   end
-  return this
-end
-
-local function remove_hashbang(script)
-  return (script:gsub("^%#%![^\n]*\n", ""))
-end
-
-local class = {}
-
-if _VERSION >= "Lua 5.2" then
-  class.searchers = package.searchers
-else
-  class.searchers = package.loaders
-end
-
-function class.new()
-  return class.save({
-    stack = sequence():push({
-      require = sequence();
-      loader = sequence();
-    })
-  })
-end
-
-function class:save()
-  self.context = {
-    arg = copy({}, arg);
-    searchers = copy({}, class.searchers);
-    require = require;
-    exit = os.exit;
-  }
-  return self
-end
-
-function class:restore()
-  local context = self.context
-  copy(arg, context.arg)
-  copy(class.searchers, context.searchers)
-  require = context.require
-  os.exit = context.exit
-end
-
-function class:parse_arg()
-  local out = sequence_writer()
-  local output
-
-  arg[-1] = "lua"
-  arg[0] = nil
-
-  local i = 1
-  local n = #arg
-  while i <= n do
-    local a, b = arg[i], arg[i + 1]
-    local c = a:match("^%-(.*)")
-    if c == nil then
-      local handle = assert(io.open(a))
-      out:write(remove_hashbang(handle:read("*a")))
-      arg[0] = a
-      i = i + 1
-    else
-      if c == "e" then
-        out:write(b, "\n")
-        i = i + 2
-      elseif c == "l" then
-        out:write(("_ENV[%q] = require(%q)\n"):format(b, b))
-        i = i + 2
-      elseif c == "-" then
-        i = i + 1
-        break
-      elseif c == "" then
-        out:write(remove_hashbang(io.read("*a")))
-        arg[0] = "-"
-        i = i + 1
-        break
-      elseif c == "o" then
-        output = b
-        i = i + 2
-      else
-        error("unrecognized option '" .. a .. "'")
-      end
-    end
-  end
-
-  local j = 0
-  for i = i, n do
-    j = j + 1
-    arg[j] = arg[i]
-  end
-  for i = j + 1, n do
-    arg[i] = nil
-  end
-
-  self.script = out:concat()
-  self.output = output
-  return self
+  repeat
+    code, n = code:gsub("^%-%-[^\n]*\n+", "")
+  until n == 0
+  return code
 end
 
 local function searcher(stack, modname, loader, filename, ...)
@@ -140,10 +66,90 @@ local function searcher(stack, modname, loader, filename, ...)
   return loader, filename, ...
 end
 
+local function write(out, u)
+  for v in u.require:each() do
+    write(out, v)
+  end
+  for v in u.loader:each() do
+    out:write(("package.loaded[%q] = (function ()\n"):format(v.modname))
+    out:write(clean(read_file(v.filename)))
+    out:write("end)()\n")
+  end
+end
+
+local class = {}
+
+function class.new()
+  return {
+    stack = sequence():push({
+      require = sequence();
+      loader = sequence();
+    })
+  }
+end
+
+function class:setup_arg()
+  local out = sequence_writer()
+
+  local i = 1
+  local n = #arg
+  while i <= n do
+    local a, b = arg[i], arg[i + 1]
+    local c = a:match("^%-(.*)")
+    if c == nil then
+      local handle = assert(io.open(a))
+      out:write(clean(handle:read("*a")))
+      script = a
+      i = i + 1
+      break
+    else
+      if c == "e" then
+        out:write(b, "\n")
+        i = i + 2
+      elseif c == "l" then
+        out:write(("_G[%q] = require(%q)\n"):format(b, b))
+        i = i + 2
+      elseif c == "-" then
+        i = i + 1
+        break
+      elseif c == "" then
+        out:write(clean(io.read("*a")))
+        script = "-"
+        i = i + 1
+        break
+      elseif c == "o" then
+        self.output = b
+        i = i + 2
+      else
+        error("unrecognized option '" .. a .. "'")
+      end
+    end
+  end
+
+  arg[-1] = "lua"
+  arg[0] = script
+  local j = 0
+  for i = i, n do
+    j = j + 1
+    arg[j] = arg[i]
+  end
+  for i = j, n do
+    arg[i] = nil
+  end
+
+  if empty(out) then
+    return nil
+  else
+    local script = out:concat()
+    self.script = script
+    return script
+  end
+end
+
 function class:setup_searchers()
   local stack = self.stack
-  for k, v in pairs(class.searchers) do
-    class.searchers[k] = function (modname, ...)
+  for k, v in pairs(searchers) do
+    searchers[k] = function (modname, ...)
       return searcher(stack, modname, v(modname, ...))
     end
   end
@@ -152,8 +158,8 @@ end
 
 function class:setup_require()
   local stack = self.stack
-  local context = self.context
-  require = function (modname)
+  local require = require
+  _G.require = function (modname)
     local this = {
       modname = modname;
       require = sequence();
@@ -161,49 +167,39 @@ function class:setup_require()
     }
     stack:top().require:push(this)
     stack:push(this)
-    local result = context.require(modname)
+    local result = require(modname)
     stack:pop()
     return result
   end
-  return self
 end
 
 function class:setup_exit()
-  local context = self.context
+  local exit = os.exit
   os.exit = function (code)
     if code == nil or code == 0 then
       error(exit_success)
     else
-      context.exit(code)
+      exit(code)
     end
   end
   return self
 end
 
 function class:eval()
+  dromozoa_amalgamate_loading = true
   local result, message = pcall(assert(loadstring(self.script)), unpack(arg))
+  dromozoa_amalgamate_loading = nil
   if not result and message ~= exit_success then
     error(message)
   end
   return self
 end
 
-local function write(out, this)
-  for v in this.require:each() do
-    write(out, v)
-  end
-  for v in this.loader:each() do
-    out:write("dromozoa_amalgamate_loaded = true\n")
-    out:write(("package.loaded[%q] = (function ()\n"):format(v.modname))
-    out:write(remove_hashbang(read_file(v.filename)))
-    out:write("end)()\n")
-  end
-end
-
 function class:write(out)
-  out:write("#! /usr/bin/env lua")
+  out:write("#! /usr/bin/env lua\n", prolog)
   write(out, self.stack:top())
   out:write(self.script)
+  return out
 end
 
 local metatable = {
